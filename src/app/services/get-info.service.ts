941cc516fef27db732e0689443620cab7644f17b
Blacknred <andyblacknred@gmail.com>
2019-11-10 14:22:13 +0200

    готово

--- src/app/services/get-info.service.ts
+++ src/app/services/get-info.service.ts
@@ -8,8 +8,11 @@ export class GetInfoService {
 
   constructor(private http: HttpClient) { }
 
-  private listWithObjects: Object = {};
-  private infoAboutObject: Object = [];
+  private listWithObjects: any = {results: []};
+  private infoAboutObject: Object;
+  private listWithObjectsResults: any;
+  private sortDirectionReverse: boolean = false;
+  private categoryUrl: string;
 
   getNavList = () => {
     let categories: Array<{name: string, link: string}> = [];
@@ -21,14 +24,46 @@ export class GetInfoService {
     return categories;
   };
 
+  sort = () => {
+    this.listWithObjectsResults.sort(function(a: any, b: any) {
+      var textA = a.name.toUpperCase() || a.title.toUpperCase();
+      var textB = b.name.toUpperCase() || b.title.toUpperCase();
+      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
+    if (this.sortDirectionReverse) {
+      this.listWithObjectsResults.reverse();
+    }
+  };
+
+  changeSortDirection = () => {
+    this.sortDirectionReverse = !this.sortDirectionReverse;
+    this.sort();
+  };
+
   getListWithObjects = (event) => {
-    if (event.target.getAttribute('data-link') != null) {
-      this.http.get(event.target.getAttribute('data-link')).subscribe( (res) => {
+    this.categoryUrl = event.target.getAttribute('data-link');
+    if (this.categoryUrl != null) {
+      this.http.get(this.categoryUrl).subscribe( (res: any) => {
         this.listWithObjects = res;
-        console.log(res);
+        this.listWithObjectsResults = res.results || [];
+        this.sort();
         return res;
       })
     }
+    event.target.parentElement.querySelectorAll('li').forEach(function (listItem) {
+       listItem.classList.remove('active');
+    });
+    event.target.classList.add('active');
+  };
+
+  search = (event) => {
+    let url = `${this.categoryUrl}?search=${event.target.value}`;
+    this.http.get(url).subscribe( (res: any) => {
+      this.listWithObjects = res;
+      this.listWithObjectsResults = res.results || [];
+      this.sort();
+      return res;
+    })
   };
 
   getInfoAboutObject = (event) => {
